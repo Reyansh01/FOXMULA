0529b4b4d006e2bde61229edbd18f384f93b7307
Reyansh Mishra <51244218+Reyansh01@users.noreply.github.com>
2021-05-27 17:52:48 +0530

    updated Quadrilateral.java

--- src/com/foxmula/assignment01/Quadrilateral.java
+++ src/com/foxmula/assignment01/Quadrilateral.java
@@ -5,14 +5,14 @@ public class Quadrilateral {
     protected int x1,y1,x2,y2,x3,y3,x4,y4;
     void points(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4)
     {
-        this.x1=x1;
-        this.y1=y1;
-        this.x2=x2;
-        this.y2=y2;
-        this.x3=x3;
-        this.y3=y3;
-        this.x4=x4;
-        this.y4=y4;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+        this.x4 = x4;
+        this.y4 = y4;
     }
     public static void main(String[] args)
     {
